ece56834860fd37d64c8db87750c1407931f1f16
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2018-10-09 09:08:04 +0700

    Fix popup view after remove public

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from 'Components/pop-up/App';
 import registerServiceWorker from './registerServiceWorker';
 
-function run() {
+// function run() {
     ReactDOM.render(<App />, document.getElementById('root'));
     registerServiceWorker();
-}
+// }
 
-const loadedStates = ['complete', 'loaded', 'interactive'];
+// const loadedStates = ['complete', 'loaded', 'interactive'];
 
-if (loadedStates.includes(document.readyState) && document.body) {
-    run();
-} else {
-    window.addEventListener('DOMContentLoaded', run, false);
-}
+// if (loadedStates.includes(document.readyState) && document.body) {
+//     run();
+// } else {
+//     window.addEventListener('DOMContentLoaded', run, false);
+// }
